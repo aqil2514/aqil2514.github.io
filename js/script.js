bba0d10a852e82b1c72e3cee61c2aef2274bfd8b
aqil2514 <muhamadaqil383@gmail.com>
2023-03-26 08:33:52 +0700

    Test

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ const header = document.querySelector('.navbar');
 const sb = document.querySelector('.portfolio select');
 const dPage = document.getElementById('default');
 const wdPage = document.getElementById('web-designer');
+const pPage = document.getElementById('penerjemah');
+const vePage = document.getElementById('video-editor');
+const pePage = document.getElementById('penulis')
 const btnTampilkan = document.getElementById('but-bidang');
 
 //Navbar akan muncul ketika tombol menu ditekan
@@ -24,8 +27,32 @@ btnTampilkan.addEventListener('click', function(){
     if(sb[0].selected){
         dPage.style.display ='block';
         wdPage.style.display='none';
+        pPage.style.display='none'
+        vePage.style.display='none'
+        pePage.style.display='none'
     }else if(sb[1].selected){
         wdPage.style.display='flex';
         dPage.style.display='none';
+        pPage.style.display='none'
+        vePage.style.display='none'
+        pePage.style.display='none'
+    }else if(sb[2].selected){
+        wdPage.style.display='none';
+        dPage.style.display='none';
+        pPage.style.display='flex'
+        vePage.style.display='none'
+        pePage.style.display='none'
+    }else if(sb[3].selected){
+        wdPage.style.display='none';
+        dPage.style.display='none';
+        pPage.style.display='none'
+        vePage.style.display='flex'
+        pePage.style.display='none'
+    }else if(sb[4].selected){
+        wdPage.style.display='none';
+        dPage.style.display='none';
+        pPage.style.display='none'
+        vePage.style.display='none'
+        pePage.style.display='flex'
     }
 })
